4557a525e5a2900d2ce47f6b38c8aa6ff13a6866
Chetan Borse <chetanborse2106@gmail.com>
2016-12-16 15:20:41 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# CREATE FOLDER FOR BINARIES
+mkdir bin
+
+
 # COMPILE JAVA SOURCE CODE
 javac -cp ".:./ext/lucene-analyzers-common-6.2.1.jar:./ext/lucene-core-6.2.1.jar:./ext/lucene-queryparser-6.2.1.jar" ./src/org/myorg/ResumeMatcher/*.java -d bin -Xlint
 
@@ -8,3 +12,4 @@ jar -cvf ResumeMatcher.jar -C bin/ .
 
 # RUN RESUME MATCHER APPLICATION
 java -cp .:ext/lucene-analyzers-common-6.2.1.jar:ext/lucene-core-6.2.1.jar:./ext/lucene-queryparser-6.2.1.jar:ResumeMatcher.jar org.myorg.ResumeMatcher.ResumeMatcher "./inverted_index" "./input" "java"
+
